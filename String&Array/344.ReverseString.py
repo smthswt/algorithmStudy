cde7ca1406df9dee66e77a8fdd817bd572a9f160
smthswt <hin19823@gmail.com>
2025-03-26 19:34:42 +0900

    first commit

---
+++ String&Array/344.ReverseString.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/reverse-string/
+from typing import List
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        s.reverse()
+        print(s)
